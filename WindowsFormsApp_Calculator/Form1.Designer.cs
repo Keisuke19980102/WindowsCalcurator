9010418d6709b7737b74aab07696cdc29e3cf0a1
unknown <kei19980102abc@gmail.com>
2020-11-15 13:59:41 +0900

    first comment

---
+++ WindowsFormsApp_Calculator/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApp_Calculator
+{
+    partial class Display
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.add = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.error = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.add.Location = new System.Drawing.Point(12, 160);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(50, 50);
+            this.add.TabIndex = 0;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // minus
+            // 
+            this.minus.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.minus.Location = new System.Drawing.Point(76, 160);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(50, 50);
+            this.minus.TabIndex = 1;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // multiply
+            // 
+            this.multiply.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.multiply.Location = new System.Drawing.Point(144, 160);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(50, 50);
+            this.multiply.TabIndex = 2;
+            this.multiply.Text = "×";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // divide
+            // 
+            this.divide.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.divide.Location = new System.Drawing.Point(216, 160);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(50, 50);
+            this.divide.TabIndex = 3;
+            this.divide.Text = "÷";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.result.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.result.Location = new System.Drawing.Point(79, 22);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(33, 35);
+            this.result.TabIndex = 4;
+            this.result.Text = "0";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(76, 89);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 19);
+            this.textBox.TabIndex = 5;
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.Location = new System.Drawing.Point(85, 238);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(0, 12);
+            this.error.TabIndex = 6;
+            // 
+            // Display
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.error);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.add);
+            this.Name = "Display";
+            this.Text = "Calcurator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label error;
+    }
+}
+
---
+++ WindowsFormsApp_Calculator/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Calculator
+{
+    public partial class Display : Form
+    {
+        public int input = 0;
+        public double output = 0;
+        public Display()
+        {
+            InitializeComponent();
+        }
+
+        private void read_input()
+        {
+            if(int.TryParse(textBox.Text,out input))
+            {
+                error.Text = "";
+            }
+            else
+            {
+                error.Text = "整数を入力してください";
+            }
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            read_input();
+            output += input;
+            result.Text = output.ToString();
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            read_input();
+            output -= input;
+            result.Text = output.ToString();
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            read_input();
+            output *= input;
+            result.Text = output.ToString();
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            if (input != 0)
+            {
+                read_input();
+                output /= input;
+                result.Text = output.ToString();
+            }
+            else
+            {
+                error.Text = "Infinity";
+            }
+        }
+    }
+}
